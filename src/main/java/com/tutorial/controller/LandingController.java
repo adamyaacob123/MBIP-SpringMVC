ae0060cdf00e54c641c20d96ef875ff57bde88e4
adamm <adamyaacob7@gmail.com>
2023-11-26 23:39:36 +0800

    first commit

---
+++ src/main/java/com/tutorial/controller/LandingController.java
@@ -0,0 +1,34 @@
+package com.tutorial.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+@Controller
+@RequestMapping("/greet")
+public class LandingController {
+
+	@RequestMapping("/welcome/{facultyName}/{userName}")
+	protected ModelAndView helloWorld(@PathVariable("facultyName") String faculty,
+			@PathVariable("userName") String name) {
+		ModelAndView modelandview = new ModelAndView("HelloPage");
+		modelandview.addObject("welcomeMessage", "Hi" + name + " Your Faculty is: " + faculty);
+		return modelandview;
+	}
+
+	@RequestMapping("/selamat")
+	protected ModelAndView selamatDatang() {
+		ModelAndView modelandview = new ModelAndView("HelloPage");
+		modelandview.addObject("welcomeMessage", "Selamat Datang with annotation! ");
+		return modelandview;
+	}
+
+	@RequestMapping("/landing")
+	protected ModelAndView Landing() {
+		ModelAndView modelandview = new ModelAndView("LandingPage");
+		modelandview.addObject("welcomeMessage", "Selamat Datang with annotation! ");
+		return modelandview;
+	}
+}
